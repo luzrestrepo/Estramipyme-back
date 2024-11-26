e440c38ecd86053c0efff66b9d30338386d77a0a
Cuoralex <alexacuartas@gmail.com>
2024-11-26 05:30:44 -0500

    Guardar cambios WebConfig, conexion backend y frontend

--- src/main/java/com/project/config/WebConfig.java
+++ src/main/java/com/project/config/WebConfig.java
@@ -7,13 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
- //Configuramos el CORS, para que el backend pueda recibir peticiones desde el FrontEnd en ese puerto.
+    // Configuramos el CORS, para que el backend pueda recibir peticiones desde el
+    // FrontEnd en ese puerto.
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+
             }
         };
     }
